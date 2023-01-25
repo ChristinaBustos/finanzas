25b2be40e56cea4354c219607c9029489ae81ef6
ChristinaBustos <20213tn151@utez.edu.mx>
2023-01-25 09:46:55 -0600

    reciente

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from '@rneui/themed';
+import { initializeApp } from "firebase/app";
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyC7eejQzqpyz4XmbuiFp_9brV90h56p-cI",
+    authDomain: "finanzas-7a5b1.firebaseapp.com",
+    projectId: "finanzas-7a5b1",
+    storageBucket: "finanzas-7a5b1.appspot.com",
+    messagingSenderId: "537976435132",
+    appId: "1:537976435132:web:ccb6fbe485f6bca98a7bb9"
+  };
+  const app = initializeApp(firebaseConfig);
   return (
     <View style={styles.container}>
       <Text>Hola Anna Christina!</Text>
@@ -11,6 +21,7 @@ export default function App() {
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {

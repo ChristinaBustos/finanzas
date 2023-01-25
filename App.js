41439760d73e99dce0ed5cbc5b13a4c9729e4787
ChristinaBustos <20213tn151@utez.edu.mx>
2023-01-25 09:15:58 -0600

    nuevo

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/themed';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola Anna Christina!</Text>
+      <Button title="Solid" />
       <StatusBar style="auto" />
     </View>
   );

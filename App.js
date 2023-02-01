5ef5aded9062dd11fdcd13bc0eeff6c8c7bf9e38
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2023-02-01 09:53:11 -0600

    new

--- App.js
+++ App.js
@@ -2,23 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from '@rneui/themed';
 import { initializeApp } from "firebase/app";
+import Navigation from './config/navigation/Navigation';
 
 export default function App() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyC7eejQzqpyz4XmbuiFp_9brV90h56p-cI",
-    authDomain: "finanzas-7a5b1.firebaseapp.com",
-    projectId: "finanzas-7a5b1",
-    storageBucket: "finanzas-7a5b1.appspot.com",
-    messagingSenderId: "537976435132",
-    appId: "1:537976435132:web:ccb6fbe485f6bca98a7bb9"
-  };
-  const app = initializeApp(firebaseConfig);
   return (
-    <View style={styles.container}>
-      <Text>Hola Anna Christina!</Text>
-      <Button title="Solid" />
-      <StatusBar style="auto" />
-    </View>
+    <Navigation/>
   );
 }
 
